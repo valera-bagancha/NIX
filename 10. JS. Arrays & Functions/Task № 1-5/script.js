98f9568687e0ce7bcb706b95286c684859018c32
valera-bagancha <valerabagancha@gmail.com>
2022-09-25 16:26:44 +0300

    10 lesson

---
+++ 10. JS. Arrays & Functions/Task № 1-5/script.js
@@ -0,0 +1,19 @@
+// Task №1
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// Task №2
+// function multiplyNumeric(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] == 'number') {
+//       obj[key] *= 2;
+//     }
+//   }
+// }
+
+// Task №3
+console.log('hello');

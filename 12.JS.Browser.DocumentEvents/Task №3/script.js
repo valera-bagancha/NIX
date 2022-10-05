93d3a08860359d7ba7b1e3921a156eab90800768
valera-bagancha <valerabagancha@gmail.com>
2022-10-05 20:46:56 +0300

    12Lesson

--- 12.JS.Browser.DocumentEvents/Task №3/script.js
+++
@@ -1,6 +0,0 @@
-const table = document.body.firstElementChild;
-
-    for (let i = 0; i < table.rows.length; i++) {
-      let row = table.rows[i];
-      row.cells[i].style.backgroundColor = 'red';
-    }
--- 12.JS.Browser.DocumentEvents/Task №5/script.js
+++
@@ -1,6 +0,0 @@
-for (let li of document.querySelectorAll('li')) {
-  let title = li.firstChild.data;
-  title = title.trim();
-  let count = li.getElementsByTagName('li').length;
-  console.log(title + ': ' + count);
-}

5658a284ca2179a78cb9495e8655762345767ba8
valera-bagancha <valerabagancha@gmail.com>
2022-10-06 10:37:07 +0300

    final

--- 13.JS.BrowserDocumentEvents/Task №7/script.js
+++ 13.JS.BrowserDocumentEvents/Task №7/script.js
@@ -1,2 +1,5 @@
+button.addEventListener("click" , () => alert ( "1" ));
+button.removeEventListener("click" , () => alert ( "1" ));
+button.onclick = () => alert ( 2 ); //Вiдповiдь 1 и 2. Перший обробник спрацює, тому що він не був видалений методом removeEventListener
+// Незважаючи на те, що код ідентичний, в removeEventListener передається нова, інша функція. Тому обробник не видаляеться
 
-}

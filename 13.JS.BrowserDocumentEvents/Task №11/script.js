ae1ebf587b69a71a17324dc5aee3026ca099b6ee
valera-bagancha <valerabagancha@gmail.com>
2022-10-09 12:09:29 +0300

    final

--- 13.JS.BrowserDocumentEvents/Task №11/script.js
+++
@@ -1,8 +0,0 @@
-let selectedOption = genres.options[genres.selectedIndex];
-console.log(selectedOption.value);
-console.log(selectedOption.text);
-
-let newOption = new Option("Классика", "classic");
-genres.append(newOption);
-
-newOption.selected = true;
--- 13.JS.BrowserDocumentEvents/Task №9/script.js
+++
@@ -1,6 +0,0 @@
-let menuElem = document.getElementById('sweeties');
-    let titleElem = menuElem.querySelector('.title');
-
-    titleElem.onclick = function() {
-      menuElem.classList.toggle('open');
-    };
